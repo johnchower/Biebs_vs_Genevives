4e0ab562e6697c48815cd8e46fe7f49d3dddc6b1
johnchower <johnchower@gmail.com>
2016-08-08 16:10:02 -0600

    Include extra parameters to allow one to select appropriate columns from data. Also include a parameter that allows user to specify directed vs. undirected

--- fn_convert_df_graph_to_igraph.R
+++ fn_convert_df_graph_to_igraph.R
@@ -1,14 +1,30 @@
 # Function: convert_df_graph_to_igraph
 # Requires packages igraph and magrittr
 
-convert_df_graph_to_igraph <- function(df){
+# cols specifies which columns hold the user ids
+
+convert_df_graph_to_igraph <- 
+  function(
+    df
+    , data_cols = 1:2
+    , connection_type_col = 3
+    ,...
+    ){
   
-  df %>%
+  is.directed <- df[, connection_type_col] %>%
+    as.character %>%
+    {sum(.!="both_ways") > 0}
+  
+  df[,data_cols] %>%
     apply(
       1
       , function(e){e}
     ) %>% 
     c %>%
-    make_graph %>%
+    make_graph(
+      ...
+      , 
+      directed = is.directed
+    ) %>%
     return
 }
