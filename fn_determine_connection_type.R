b36057fc8ed526d2577faf3cccf278a65533a83a
johnchower <johnchower@gmail.com>
2016-08-08 16:59:32 -0600

    Add fn_determine_connection_type

---
+++ fn_determine_connection_type.R
@@ -0,0 +1,5 @@
+# Function : determine_connection_type
+
+# If one of the connection types is "both_ways", then the connection type is "both_ways"
+# If there are only directional connections, but they go both ways, then output "both_ways"
+# If
