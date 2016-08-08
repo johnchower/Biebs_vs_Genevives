44e8dbaae1a48020c1f70d8a09556aadc5d65961
johnchower <johnchower@gmail.com>
2016-08-08 14:46:50 -0600

    Add fn_idvec_2_pairframe

---
+++ fn_idvec_2_pairframe.R
@@ -0,0 +1,8 @@
+# Function: idvec_2_pairframe
+
+# Converts a vector, v, to a data frame with (length(v) choose 2) rows and 2 columns.
+# Each row is a pair of elements of v.
+
+idvec_2_pairframe <- function(v){
+  
+}
