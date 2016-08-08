45309755765217eb92286da0365e3ff62461cac8
johnchower <johnchower@gmail.com>
2016-08-08 16:08:19 -0600

    Create graph_data

--- ms_etl.R
+++ ms_etl.R
@@ -1,22 +1,47 @@
 # ms_etl
 
 library(magrittr)
+library(plyr)
 library(dplyr)
 
 source('fn_load_basic_schema.r')
+source('fn_idvec_2_pairframe.r')
+source('fn_convert_df_graph_to_igraph.r')
 
 df_list <- load_basic_schema()
 
-# "Extract"
+# "Extract" ####
 user_connections <- df_list$USER_CONNECTIONS
 posts <- df_list$POSTS
 comments <- df_list$COMMENTS
-follows <- df$FOLLOWS
-space_membership <- df$SPACE_MEMBERSHIP
+follows <- df_list$FOLLOWS
+space_membership <- df_list$SPACE_MEMBERSHIP
 
-# Transform
+# Transform ####
 
-# Load
+# Create graph of user connections, follows, and shared spaces
+
+graph_connections <- user_connections %>%
+  select(user1_id = connectable1_id, user2_id =  connectable2_id) %>%
+  mutate(connection_type = "both_ways")
+
+graph_follows <- follows %>%
+  select(user1_id = followable_id, user2_id = follower_id) %>%
+  mutate(connection_type = "1_to_2")
+
+graph_spaces <- space_membership %>%
+  ddply(
+    .variables = .(membershipable_id)
+    , .fun = function(df){idvec_2_pairframe(df$member_id) %>% return}
+  ) %>%
+  select(-membershipable_id) %>%
+  rename(user1_id = minimum, user2_id = maximum) %>%
+  mutate(connection_type = "both_ways")
+
+graph_data <- rbind(graph_connections, graph_follows, graph_spaces)
+  
+
+# Load ####
 
 user_connections %>%
   write.csv(file = "user_connections.csv")
