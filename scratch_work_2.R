c3ce77d146784e29b6e688487387b8fcc7b78b19
johnchower <johnchower@gmail.com>
2016-08-09 10:27:23 -0600

    Include a code timer

--- scratch_work_2.R
+++ scratch_work_2.R
@@ -1,25 +1,28 @@
 
-
+# Draft up code here:
 
 graph_data %>%
-  arrange(desc(user1_id, user2_id, connection_type)) %>%
-  distinct %>% 
-  {
-    ddply(.
-      , .variables = colnames(.)
-      , .fun = function(df){
-        data.frame(
-          min_id = min(df$user1_id, df$user2_id)
-          , max_id = max(df$user1_id, df$user2_id)
-        )
-      }
-    ) 
-  } %>% 
-  group_by(min_id, max_id) %>%
-  summarise(
-    connection_type_0 = ifelse(
-      sum(connection_type == "both_ways") > 0
-      , "both_ways"
-      , # This is going to get complicated. Make a function.
-    )
+  ddply(
+    .variables = colnames(.)
+    , 
   )
+
+# Code timer
+
+times <- c()
+for(j in 1:50){
+  k <- 500
+  start.time <- Sys.time()
+  
+  # Put code to time in here.
+ 
+  
+  
+  end.time <- Sys.time()
+  newtimes <- (end.time - start.time)*nrow(g2)/k 
+  
+  times %<>% c(newtimes)
+}
+hist(times/60)
+mean(times)/60
+sd(times)/60
