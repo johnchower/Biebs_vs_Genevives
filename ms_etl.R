77cc219309ef058d249bd9d4e2325425cc7d2752
johnchower <johnchower@gmail.com>
2016-08-09 11:01:10 -0600

    Keep track of two-way connections by reverse-doubling the graph_connections, graph_follows, graph_spaces data frames as they are created

--- ms_etl.R
+++ ms_etl.R
@@ -24,11 +24,15 @@ space_membership <- df_list$SPACE_MEMBERSHIP
 
 graph_connections <- user_connections %>%
   select(user1_id = connectable1_id, user2_id =  connectable2_id) %>%
-  mutate(connection_type = "both_ways")
+  {
+    data.frame(
+      user1_id = c(.$user1_id, .$user2_id)
+      , user2_id = c(.$user2_id, .$user1_id)
+    )
+  }
 
 graph_follows <- follows %>%
-  select(user1_id = followable_id, user2_id = follower_id) %>%
-  mutate(connection_type = "1_to_2")
+  select(user1_id = followable_id, user2_id = follower_id) 
 
 graph_spaces <- space_membership %>%
   ddply(
@@ -37,24 +41,16 @@ graph_spaces <- space_membership %>%
   ) %>%
   select(-membershipable_id) %>%
   rename(user1_id = minimum, user2_id = maximum) %>%
-  mutate(connection_type = "both_ways")
+  {
+    data.frame(
+      user1_id = c(.$user1_id, .$user2_id)
+      , user2_id = c(.$user2_id, .$user1_id)
+    )
+  }
 
 graph_data <- rbind(graph_connections, graph_follows, graph_spaces) %>%
-  distinct %>%
-  ddply(
-    .variables = colnames(.)
-    , .fun = function(df){
-      data.frame(
-        min_id = min(df$user1_id, df$user2_id)
-        , max_id = max(df$user1_id, df$user2_id)
-      )
-    }
-  ) %>%
-  ddply(
-    .variables = .(min_id, max_id)
-    , .fun = determine_connection_type
-  ) %>% 
-  select(-min_id, -max_id)
+  distinct %>% 
+  arrange(user1_id, user2_id)
 
 # Create data frame that contains all info necessary to calculate the energy score ####
 
