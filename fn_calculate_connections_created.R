7368fdbbe15c4ba7dcbfbbd80a507ecda309f9e6
johnchower <johnchower@gmail.com>
2016-08-11 16:01:15 -0600

    Modify ot account for membershipable_id

--- fn_calculate_connections_created.R
+++ fn_calculate_connections_created.R
@@ -13,26 +13,42 @@ calculate_connections_created <- function(df){
     filter(floor(as.numeric(created_at)) == D) %>%
     mutate(membershipable_id = -1, connection_type = "follow")
   
+  graph_timeline_audience_size <- rbind(graph_connections_D, graph_follows_D) %>%
+    group_by(user1_id) %>%
+    summarise(
+      connections_created = length(unique(user2_id))
+    ) %>% 
+    mutate(membershipable_id = -1)
+  
   graph_spaces_D <- graph_spaces_with_membershipableid %>%
     filter(floor(as.numeric(created_at)) == D) %>%
     mutate(connection_type = "space")
   
-  graph_connections_for_posts <- rbind(graph_connections_D, graph_follows_D, graph_spaces_D) %>% 
-    group_by(user1_id) %>%
+  graph_spaces_audience_size <- graph_spaces_D %>%
+    group_by(user1_id, membershipable_id) %>%
     summarise(
-      timeline_connections_created = length(unique(user2_id[connection_type %in% timeline_connection_types]))
-      , space_connections_created = length(unique(user2_id[connection_type %in% space_connection_types]))
+      connections_created_space = length(unique(user2_id))
     ) %>%
-    rename(user_id = user1_id)
-  
-  # 3
-  
-  # user_postabletype_date_connectionscreated_D <- rbind(graph_connections_for_space_posts, graph_connections_for_timeline_posts) %>%
-  #   group_by(user1_id, postable_type, membershipable_id) %>%
-  #   summarise(
-  #     connections_created = length(unique(user2_id))
-  #   ) %>%
-  #   mutate(date = D)
-  
-  return(graph_connections_for_posts)
+    merge(
+      select(graph_timeline_audience_size, user1_id, connections_created.y = connections_created)
+      , by = "user1_id"
+      , all.x = T
+    ) %>%
+    mutate(
+      connections_created_timeline = 
+        ifelse(is.na(connections_created.y), 0, connections_created.y)
+      , connections_created = connections_created_space + connections_created_timeline
+    ) %>%
+    select(user1_id, membershipable_id, connections_created) %>%
+    
+    rbind(graph_timeline_audience_size, graph_spaces_audience_size) %>%
+      mutate(
+        postable_type =
+          ifelse(
+            membershipable_id == -1
+            , "Timeline"
+            , "Space"
+          )
+      ) %>% 
+    return
 }
