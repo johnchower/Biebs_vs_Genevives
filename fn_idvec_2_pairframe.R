3e16dc2a663a09ee42b02a4dbe841f6439cd578f
johnchower <johnchower@gmail.com>
2016-08-11 12:16:14 -0600

    Modify fn_idvec_2_pairframe.r so that it plays well with dplyr::do

--- fn_idvec_2_pairframe.R
+++ fn_idvec_2_pairframe.R
@@ -4,20 +4,26 @@
 # Each row is a pair of elements of v.
 
 idvec_2_pairframe <- function(df){
-  df %>%
-    select(member_id, created_at) %>% 
-    {
-      merge(
-        x = .
-        , y = .
-        , by = c()
-      )
-    } %>% 
-    filter(member_id.x < member_id.y) %>%
-    distinct(member_id.x, member_id.y, .keep_all = T) %>% 
-    group_by(member_id.x, member_id.y) %>% 
-    mutate(created_at = max(created_at.x, created_at.y)) %>% 
-    ungroup %>% 
-    select(user1_id = member_id.x, user2_id = member_id.y, created_at) %>% 
-    return
+  
+  if(nrow(df) <= 1){
+    return(data.frame())
+  } else{
+    df %>%
+      select(member_id, created_at) %>% 
+      {
+        merge(
+          x = .
+          , y = .
+          , by = c()
+        )
+      } %>% 
+      filter(member_id.x < member_id.y) %>%
+      distinct(member_id.x, member_id.y, .keep_all = T) %>% 
+      group_by(member_id.x, member_id.y) %>% 
+      mutate(created_at = max(created_at.x, created_at.y)) %>% 
+      ungroup %>% 
+      select(user1_id = member_id.x, user2_id = member_id.y, created_at) %>% 
+      mutate(membershipable_id = df$membershipable_id[1]) %>%
+      return 
+  }
 }
