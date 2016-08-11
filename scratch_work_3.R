66ef47e23e6bd95c091e9cbe47f0b2898de3420d
johnchower <johnchower@gmail.com>
2016-08-11 14:33:00 -0600

    Work on calculating the potential audience size of a post.

--- scratch_work_3.R
+++ scratch_work_3.R
@@ -1,23 +1,34 @@
 # Testing the post_audience calculation
 
+source('fn_calculate_connections_created.r')
+
 earliest_connection <- graph_data %>%
   {.$created_at} %>%
   min %>%
-  floor
+  floor 
 
-D <- earliest_connection + 365
+latest_connection <- graph_data %>%
+  {.$created_at} %>%
+  max %>%
+  floor 
 
-graph_connections_D <- graph_connections %>% 
-  filter(floor(as.numeric(created_at)) <= D)
-
-graph_follows_D <- graph_follows %>%
-  filter(floor(as.numeric(created_at)) <= D)
-
-graph_spaces_D <- graph_spaces %>%
-  filter(floor(as.numeric(created_at)) <= D)
+date_sequence <- data.frame(date = earliest_connection:latest_connection)
 
 
-graph_data_D <- rbind(graph_connections_D, graph_follows_D, graph_spaces_D) %>% 
-  distinct %>% 
-  group_by(user1_id, user2_id) %>%
-  summarise(created_at = min(created_at))
+user_date_connectionscreated <- date_sequence %>% 
+  group_by(date) %>%
+  do(calculate_connections_created(.)) %>% 
+  melt(
+    id.vars = c("date", "user_id")
+    , variable.name = "postable_type"
+    , value.name = "number_of_connections_created"
+  ) %>%
+  mutate(
+    postable_type_lower = gsub("_connections_created", "", postable_type)
+    , postable_type = 
+        paste0(
+          toupper(substr(postable_type_lower, 1, 1))
+          , substr(postable_type_lower, 2, nchar(postable_type_lower))
+        )
+  ) %>%
+  select(-postable_type_lower)
