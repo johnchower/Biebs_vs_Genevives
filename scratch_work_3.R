a62fb3f43f9db56aecd9259b6269f4e0c058f1a4
johnchower <johnchower@gmail.com>
2016-08-17 16:52:46 -0600

    Fix while loop

--- scratch_work_3.R
+++ scratch_work_3.R
@@ -17,18 +17,58 @@ date_sequence <- data.frame(date = earliest_connection:latest_connection)
 
 user_date_connectionscreated <- date_sequence %>% 
   group_by(date) %>%
-  do(calculate_connections_created(.)) %>% 
-  melt(
-    id.vars = c("date", "user_id")
-    , variable.name = "postable_type"
-    , value.name = "number_of_connections_created"
-  ) %>%
+  do(calculate_connections_created(.)) %>%
+  rename(user_id = user1_id) 
+
+user_date_connectionscreated %>%
+  group_by(user_id, membershipable_id, postable_type) %>%
+  arrange(date) %>%
+  mutate(existing_connections = cumsum(connections_created)) %>%
+  ungroup %>%
+  arrange(user_id, membershipable_id, postable_type, date) %>%
+  View
+
+user_date_connectionsexisting <- user_date_connectionscreated %>%
+  group_by(user_id, membershipable_id, postable_type) %>%
+  arrange(date) %>%
+  mutate(connections_existing = cumsum(connections_created)) %>% ungroup %>%
+  select(-connections_created) %>%
+  mutate(space_id = membershipable_id)
+
+posts %<>%
   mutate(
-    postable_type_lower = gsub("_connections_created", "", postable_type)
-    , postable_type = 
-        paste0(
-          toupper(substr(postable_type_lower, 1, 1))
-          , substr(postable_type_lower, 2, nchar(postable_type_lower))
-        )
-  ) %>%
-  select(-postable_type_lower)
+    date = floor(as.numeric(created_at))
+    , space_id = ifelse(postable_type == "Timeline", -1, postable_id)
+  )
+
+start.time <- Sys.time()
+post_reach_0 <- posts %>%
+   left_join(user_date_connectionsexisting, by = c("postable_type", "user_id", "date", "space_id"))
+
+post_reach_na <- post_reach_0 %>%
+  filter(is.na(connections_existing))
+
+post_reach <- post_reach_0 %>% filter(!is.na(connections_existing))
+
+num_iterations <- 0
+while(nrow(post_reach_na) > 0){
+  post_reach_i <- post_reach %>%
+    mutate(date = date - 1) %>%
+    select(-connections_existing) %>%
+    left_join(user_date_connectionsexisting, by = c("postable_type", "user_id", "date", "space_id"))
+  
+  post_reach_na <- post_reach_i %>%
+    filter(is.na(connections_existing))
+  
+  post_reach <- post_reach_i %>% 
+    filter(!is.na(connections_existing))
+  
+  num_iterations %<>% {. + 1}
+  print(num_iterations)
+}
+
+end.time <- Sys.time() 
+end.time-start.time
+
+   
+   
